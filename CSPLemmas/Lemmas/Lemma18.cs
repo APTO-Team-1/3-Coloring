0edd80f44358fce73e2af19d788499ab7b41a01c
Michał Klewicki <01142176@pw.edu.pl>
2021-05-08 19:51:53 +0200

    lemma 18 and 19 half way done

---
+++ CSPLemmas/Lemmas/Lemma18.cs
@@ -0,0 +1,66 @@
+﻿using CSP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSPLemmas
+{
+    public static partial class CSPLemmas
+    {
+        public static List<CspInstance> Lemma18(CspInstance instance)
+        {
+            List<CspInstance> result = new();
+
+            void Lemma18Internal(CspInstance instance)
+            {
+                List<Pair> TwoComponent = new();
+                foreach (Variable var in instance.Variables)
+                {
+                    foreach (Color color in var.AvalibleColors)
+                    {
+                        TwoComponent = new();
+                        Color currColor = color;
+                        Color lastColor = color;
+                        Pair[] tempList = new Pair[2];
+                        if (currColor.Restrictions.Count == 2)
+                        {
+                            TwoComponent.Add(currColor.Restrictions.First());
+                            currColor = currColor.Restrictions.First().Color;
+                        }
+                        while (currColor.Restrictions.Count == 2)
+                        {
+                            tempList = currColor.Restrictions.ToArray();
+                            if (tempList[0].Color != lastColor)
+                            {
+                                TwoComponent.Add(tempList[0]);
+                                lastColor = currColor;
+                                currColor = tempList[0].Color;
+                            }
+                            else
+                            {
+                                TwoComponent.Add(tempList[1]);
+                                lastColor = currColor;
+                                currColor = tempList[1].Color;
+                            }
+                            if (currColor == color)
+                            {
+                                if (TwoComponent.Count == 4) // cykl długości cztery po czterech variablach
+                                {
+
+                                }
+                                else if (TwoComponent.Count > 4)
+                                {
+
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ CSPLemmas/Lemmas/Lemma19.cs
@@ -0,0 +1,52 @@
+﻿using CSP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using GraphLib.Definitions;
+using GraphLib.Algorithms;
+
+namespace CSPLemmas
+{
+    public static partial class CSPLemmas
+    {
+        public static void Lemma19(CspInstance instance)
+        {
+            var Small2Components = FindAll2SmallComponents(instance);
+            var Good3Components = FindAllGood3Components(instance);
+            Dictionary<int, int> idsToIndex = new();
+            int index = 0;
+            foreach(Variable var in instance.Variables)
+            {
+                idsToIndex.Add(var.Id, index);
+                index++;
+            }
+
+            BipartieGraph g = new(instance.Variables.Count, Small2Components.Count + Good3Components.Count);
+            for(int i =0;i<Small2Components.Count;i++)
+            {
+                g.AddEdge(idsToIndex[Small2Components[i].Item1.Id], i);
+                g.AddEdge(idsToIndex[Small2Components[i].Item2.Id], i);
+                g.AddEdge(idsToIndex[Small2Components[i].Item3.Id], i);
+            }
+            for (int i = 0; i < Good3Components.Count; i++)
+            {
+                g.AddEdge(idsToIndex[Good3Components[i].Item1.Id], i + Small2Components.Count);
+                g.AddEdge(idsToIndex[Good3Components[i].Item2.Id], i + Small2Components.Count);
+                g.AddEdge(idsToIndex[Good3Components[i].Item3.Id], i + Small2Components.Count);
+                g.AddEdge(idsToIndex[Good3Components[i].Item4.Id], i + Small2Components.Count);
+            }
+            int[] result = new BipartieGraphMaxMatching().FindMaxMatching(g);
+        }
+
+        private static List<(Variable, Variable, Variable)> FindAll2SmallComponents(CspInstance instance)
+        {
+            List<(Variable, Variable, Variable)> ret = new();
+            return ret;
+        }
+        private static List<(Variable, Variable, Variable, Variable)> FindAllGood3Components(CspInstance instance)
+        {
+            List<(Variable, Variable, Variable, Variable)> ret = new();
+            return ret;
+        }
+    }
+}
